6f29419418e78bc5570dc4be93d15835741af293
LeoLiao-Agoy <leo211liao@gmail.com>
2021-07-18 11:24:21 -0700

    debug

--- World2D.cpp
+++ World2D.cpp
@@ -8,29 +8,17 @@
 World2D::World2D(int width, int height)
 	: width_(width), height_(height) {
 	// 10 critters at random location
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < 1000; i++) {
 		WorldCritter wc = {new RollBug(),
 			new CritterState({
-				static_cast<float>(rand() % width_ * 0 + i * 30), 
-				static_cast<float>(rand() % height_ * 0 + 100)
+				static_cast<float>(rand() % width_), 
+				static_cast<float>(rand() % height_)
 			})};
-		// wc.critter_state->v.x = static_cast<float>(rand() % 100 - 50) / 1000;
-		// wc.critter_state->v.y = static_cast<float>(rand() % 100 - 50) / 1000;
+		wc.critter_state->v.x = static_cast<float>(rand() % 100 - 50) / 100;
+		wc.critter_state->v.y = static_cast<float>(rand() % 100 - 50) / 100;
 		wc.critter_state->radius = 5;
 		critters.push_back(wc);
 	}
-
-	
-	WorldCritter wc = {new RollBug(), new CritterState({400, 100})};
-		// wc.critter_state->v.y = 0.25f;
-		// wc.critter_state->v.x = 0;
-		wc.critter_state->radius = 50;
-	// critters.push_back(wc);
-	WorldCritter wc2 = {new RollBug(), new CritterState({400, 700})};
-		wc2.critter_state->v.y = -0.25f;
-		wc2.critter_state->v.x = 0;
-		wc2.critter_state->radius = 50;
-	// critters.push_back(wc2);	
 }
 
 void World2D::render(sf::RenderWindow& window) const {
@@ -58,23 +46,24 @@ void World2D::update() {
 			CritterState * st2 = wc2.critter_state;
 			// DBUG(sqDist(wc.critter_state->p, wc2.critter_state->p));
 			float sqD = sqDist(wc.critter_state->p, wc2.critter_state->p);
-			if (sqD < (st->radius + st2->radius) * (st->radius + st2->radius)) {
+			float touch_dist = st->radius + st2->radius;
+			if (sqD < (touch_dist) * (touch_dist)) {
 				// !
 				vec2 d12 = normalize(st2->p - st->p);
-				float v12_mag =  (st-> v * d12);
-				float v21_mag = -(st2->v * d12);
+				float v12_mag =  (st-> v * d12) * 0.95f;
+				float v21_mag = -(st2->v * d12) * 0.95f;
 				// todo mass
-				if (v12_mag > 0 || true) {
-					st->v -= v12_mag * d12;
-					st2->v += v12_mag * d12;
-				}
-				if (v21_mag > 0 || true) {
-					st->v -= v21_mag * d12;
-					st2->v += v21_mag * d12;
-				}
+				st->v -= v12_mag * d12;
+				st2->v += v12_mag * d12;
+				st->v -= v21_mag * d12;
+				st2->v += v21_mag * d12;
 				// push back
-				// st->p -= d12 * 30.1f;
-				// st2->p += d12 * 30.1f;
+				float distance = magnitude(st2->p - st->p) - touch_dist;
+				if (distance < 0) {
+					distance -= 0.01f;
+					st->p += d12 * (distance / 2);
+					st2->p -= d12 * (distance / 2);
+				}
 			}
 		}
 
@@ -86,7 +75,7 @@ void World2D::update() {
 		// drag
 		// critter_state->v *= 0.9999f;
 		// gravity
-		critter_state->v += {0, 0.05f};
+		critter_state->v += {0, 0.009f};
 	}
 
 	// actaully move them
